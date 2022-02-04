cd5f10d8e26a9bd28ac577a887ced3a12a5a7da2
OlegErdniev <98027910+OlegErdniev@users.noreply.github.com>
2022-02-04 10:15:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Chetire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int n = int.Parse(Console.ReadLine());
+            n++;
+            Console.WriteLine(n);
+            Console.ReadKey();*/
+
+
+
+
+            /*Console.WriteLine("Задайте переменную а ");
+             int a = int.Parse(Console.ReadLine());
+             Console.WriteLine("Задайте переменную b ");
+             int b = int.Parse(Console.ReadLine());
+             double result = (a + b) / 2;
+
+
+
+             Console.WriteLine($"Введите два числа: {a}, {b}\nСреднее арифметическое:{result}");
+             Console.ReadKey();*/
+
+            /*Console.WriteLine("Задайте переменную а ");
+            double a = double.Parse(Console.ReadLine());
+            double P = a * 3;
+            double square = (a * a * Math.Sqrt(3)) / 4;
+
+            Console.WriteLine($"Введите сторону треугольника, пожалуйста: {a}\nПериметр треугольника:{P}\nПлощадь:{square}");
+            Console.ReadKey();*/
+
+
+
+        }
+    }
+}
